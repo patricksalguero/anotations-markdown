d6401519f796be466d0fc08a06761e57999a3135
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2018-10-07 12:32:09 -0500

    Se agrega el template material design by html

---
+++ markdown/welcome.md
@@ -0,0 +1,27 @@
+# Bienvenido al Curso de Git
+
+```css
+  .helo {
+    color:white;
+  }
+```
+
+```js
+const name = 'Patrick Armando Salguero Avalos'
+```
+
+```properties
+application.name=Spring Boot Application
+```
+
+```javascript
+const http = require('http')
+```
+
+```java
+public class Main {
+  public static void main(String args[]) {
+    System.out.printLn('Hola Patrick Salguero!!')
+  }
+}
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { MarkdownModule, MarkedOptions } from "ngx-markdown";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({
+      loader: HttpClient,
+      markedOptions: {
+        provide: MarkedOptions,
+        useValue: {
+          gfm: true,
+          tables: true,
+          breaks: false,
+          pedantic: false,
+          sanitize: false,
+          smartLists: true,
+          smartypants: false
+        }
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
